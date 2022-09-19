c36be5ed729af0931e17b8bfea859cbf937acd1e
Bartłomiej Cisek <bartlomiej.cisek@outlook.com>
2022-09-19 17:14:35 +0200

    Header.vue + Login.vue + Register.vue

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -4,7 +4,7 @@ import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
-import { Head, useForm } from '@inertiajs/inertia-vue3';
+import {Head, useForm} from '@inertiajs/inertia-vue3';
 
 defineProps({
     status: String,
@@ -21,28 +21,36 @@ const submit = () => {
 
 <template>
     <GuestLayout>
-        <Head title="Forgot Password" />
+        <Head title="Forgot Password"/>
+        <template #heading>
+            <h1 class="font-bold text-3xl">RESETOWANIE HASŁA</h1>
+        </template>
 
-        <div class="mb-4 text-sm text-gray-600">
-            Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
-        </div>
+        <template #content>
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
-
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
-                <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus autocomplete="username" />
-                <InputError class="mt-2" :message="form.errors.email" />
+            <div class="mb-4 text-sm text-gray-600">
+                Forgot your password? No problem. Just let us know your email address and we will email you a password
+                reset link that will allow you to choose a new one.
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Email Password Reset Link
-                </PrimaryButton>
+            <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+                {{ status }}
             </div>
-        </form>
+
+            <form @submit.prevent="submit">
+                <div>
+                    <InputLabel for="email" value="Email"/>
+                    <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus
+                               autocomplete="username"/>
+                    <InputError class="mt-2" :message="form.errors.email"/>
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                        Email Password Reset Link
+                    </PrimaryButton>
+                </div>
+            </form>
+        </template>
     </GuestLayout>
 </template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <GuestLayout>
+        <template #heading>
+            <div class="2xl:container 2xl:mx-auto flex justify-center mt-10">
+                <h1 class="text-4xl font-extrabold">Ranking kredytów hipotecznych</h1>
+            </div>
+        </template>
+    </GuestLayout>
+</template>
+
+
+<script setup>
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+</script>
+
