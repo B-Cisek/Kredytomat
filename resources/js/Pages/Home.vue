c4db90d585943abfaa8325247df6e848e1bcf8ea
Bartłomiej Cisek <bartlomiej.cisek@outlook.com>
2022-09-23 17:29:35 +0200

    Header component

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <Header />
+</template>
+
+<script>
+
+
+import Header from "@/Components/Header.vue";
+export default {
+    name: "Home",
+    components: {Header},
+}
+</script>
+
+<style scoped>
+
+</style>
